c05587444bb7593aee994b1c7ca107039b9f6013
yanliang567 <yanliang.qiao@zilliz.com>
2021-10-23 18:55:51 +0800

    Update insert pfer
    Signed-off-by: yanliang567 <yanliang.qiao@zilliz.com>

--- tests/python_client/yanliang_ws/test_insert_perf.py
+++ tests/python_client/yanliang_ws/test_insert_perf.py
@@ -17,11 +17,12 @@ nbs = [100, 1000, 10*1000, 20*1000, 40*1000, 60*1000, 100*1000]
 dim = 128
 
 
-def insert(nb1, shard_num, threads_num, name):
+def insert(nb1, shard_num, threads_num, ins_times_per_thread, name):
 
     # create
     nb = int(nb1)
     threads_num = int(threads_num)
+    ins_times_per_thread = int(ins_times_per_thread)
     t0 = time.time()
     auto_id = FieldSchema(name="auto_id", dtype=DataType.INT64, description="auto primary id")
     age_field = FieldSchema(name="age", dtype=DataType.INT64, description="age")
@@ -46,17 +47,16 @@ def insert(nb1, shard_num, threads_num, name):
 
     # insert
     threads = []
-    ins_time_per_thread = 10
-    logging.info(f"ready to insert {name}, insert {ins_time_per_thread} times per thread")
+    logging.info(f"ready to insert {name}, insert {ins_times_per_thread} times per thread")
     if threads_num > 1:
         for i in range(threads_num):
-            t = threading.Thread(target=insert_th, args=(collection, data, int(ins_time_per_thread), i))
+            t = threading.Thread(target=insert_th, args=(collection, data, int(ins_times_per_thread), i))
             threads.append(t)
             t.start()
         for t in threads:
             t.join()
     else:
-        for r in range(ins_time_per_thread):
+        for r in range(ins_times_per_thread):
             t1 = time.time()
             res = collection.insert(data)
             t2 = time.time() - t1
@@ -71,18 +71,19 @@ if __name__ == '__main__':
     nb1 = int(sys.argv[1])      # insert nb
     shards = int(sys.argv[2])   # shards number
     th = int(sys.argv[3])       # insert thread num
-    x = int(sys.argv[4])        # x times insert with thread num
+    per_thread = int(sys.argv[4])     # insert times per thread
+    x = int(sys.argv[5])        # x times insert with thread num
     host = "10.98.0.20"
     port = 19530
     conn = connections.connect('default', host=host, port=port)
-    logging.basicConfig(filename=f"/tmp/insert_nb{nb1}_shards{shards}_threads{th}.log",
+    logging.basicConfig(filename=f"/tmp/insert_nb{nb1}_shards{shards}_threads{th}_per{per_thread}.log",
                         level=logging.INFO, format=LOG_FORMAT, datefmt=DATE_FORMAT)
 
     for i in range(x):
         nb1 = nb1 * 2
-        collection_name = f"insert_nb{nb1}_shards{shards}_threads{th}"
+        collection_name = f"insert_nb{nb1}_shards{shards}_threads{th}_per{per_thread}"
         t1 = time.time()
-        collection = insert(nb1, shards, th, collection_name)
+        collection = insert(nb1, shards, th, per_thread, collection_name)
         t2 = time.time() - t1
         logging.info(f"Insert cost {t2}, {collection_name}")
 
