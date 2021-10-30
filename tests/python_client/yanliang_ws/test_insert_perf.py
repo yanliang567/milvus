d373acb435734fb39324b82f5cbfae6b6900aab3
yanliang567 <yanliang.qiao@zilliz.com>
2021-10-30 16:14:09 +0800

    Update insert perf
    Signed-off-by: yanliang567 <yanliang.qiao@zilliz.com>

--- tests/python_client/yanliang_ws/test_insert_perf.py
+++ tests/python_client/yanliang_ws/test_insert_perf.py
@@ -11,8 +11,9 @@ LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
 DATE_FORMAT = "%m/%d/%Y %H:%M:%S %p"
 
 prefix = "ins_"
-nbs = [1, 100, 1000, 10*1000, 20*1000, 40*1000]
+nbs = [1, 100, 1000, 10*1000, 20*1000, 40*1000, 60*1000, 80*1000, 100*1000]
 dim = 128
+auto_id = False
 
 
 def insert(data, threads_num, ins_times_per_thread, collection):
@@ -21,7 +22,7 @@ def insert(data, threads_num, ins_times_per_thread, collection):
         for r in range(rounds):
             t1 = time.time()
             res = col_w.insert(data)
-            t2 = time.time() - t1
+            t2 = round(time.time() - t1, 3)
             logging.info(f"assert insert thread{thread_no} round{r}: {t2}")
 
     # insert
@@ -38,7 +39,7 @@ def insert(data, threads_num, ins_times_per_thread, collection):
         for r in range(ins_times_per_thread):
             t1 = time.time()
             res = collection.insert(data)
-            t2 = time.time() - t1
+            t2 = round(time.time() - t1, 3)
             logging.info(f"assert insert thread0 round{r}: {t2}")
 
     return collection
@@ -63,27 +64,46 @@ if __name__ == '__main__':
     for nb1 in nbs:
         collection_name = f"insert_nb{nb1}_shards{shards}_threads{th}_per{per_thread}"
         t0 = time.time()
-        auto_id = FieldSchema(name="auto_id", dtype=DataType.INT64, description="auto primary id")
-        age_field = FieldSchema(name="age", dtype=DataType.INT64, description="age")
-        embedding_field = FieldSchema(name="embedding", dtype=DataType.FLOAT_VECTOR, dim=dim)
-        schema = CollectionSchema(fields=[auto_id, age_field, embedding_field],
-                                  auto_id=True, primary_field=auto_id.name,
-                                  description="collection of insert perf")
-        collection = Collection(name=collection_name, schema=schema, shards_num=shards)
-        tt = time.time() - t0
-        logging.info(f"assert create {collection_name}: {tt}")
+        if auto_id is True:
+            collection_name = f"insert_nb{nb1}_shards{shards}_threads{th}_per{per_thread}_t"
+            id = FieldSchema(name="id", dtype=DataType.INT64, description="auto primary id")
+            age_field = FieldSchema(name="age", dtype=DataType.INT64, description="age")
+            embedding_field = FieldSchema(name="embedding", dtype=DataType.FLOAT_VECTOR, dim=dim)
+            schema = CollectionSchema(fields=[id, age_field, embedding_field],
+                                      auto_id=True, primary_field=id.name,
+                                      description="collection of insert perf")
+            collection = Collection(name=collection_name, schema=schema, shards_num=shards)
+            tt = time.time() - t0
+            logging.info(f"assert create {collection_name}: {tt}")
 
-        # prepare data
-        ages = [random.randint(1, 100) for i in range(nb1)]
-        embeddings = [[random.random() for _ in range(dim)] for _ in range(nb1)]
-        data = [ages, embeddings]
+            # prepare data
+            ages = [random.randint(1, 100) for i in range(nb1)]
+            embeddings = [[random.random() for _ in range(dim)] for _ in range(nb1)]
+            data = [ages, embeddings]
+        else:
+            collection_name = f"insert_nb{nb1}_shards{shards}_threads{th}_per{per_thread}_f"
+            id = FieldSchema(name="id", dtype=DataType.INT64, description="custom primary id")
+            age_field = FieldSchema(name="age", dtype=DataType.INT64, description="age")
+            embedding_field = FieldSchema(name="embedding", dtype=DataType.FLOAT_VECTOR, dim=dim)
+            schema = CollectionSchema(fields=[id, age_field, embedding_field],
+                                      auto_id=False, primary_field=id.name,
+                                      description="collection of insert perf")
+            collection = Collection(name=collection_name, schema=schema, shards_num=shards)
+            tt = time.time() - t0
+            logging.info(f"assert create {collection_name}: {tt}")
+
+            # prepare data
+            ids = [i for i in range(nb1)]
+            ages = [random.randint(1, 100) for i in range(nb1)]
+            embeddings = [[random.random() for _ in range(dim)] for _ in range(nb1)]
+            data = [ages, embeddings]
 
         t1 = time.time()
         collection = insert(data, th, per_thread, collection)
         t2 = time.time() - t1
-        req_per_sec = per_thread * th / t2              # how many insert requests response per second
-        entities_throughput = nb1 * req_per_sec         # how many entities inserted per second
-        logging.info(f"Insert  {collection_name} cost {t2}, "
+        req_per_sec = round(per_thread * th / t2, 3)            # how many insert requests response per second
+        entities_throughput = round(nb1 * req_per_sec, 3)         # how many entities inserted per second
+        logging.info(f"Insert  {collection_name} cost {round(t2, 3)}, "
                      f"req_per_second {req_per_sec}, entities_throughput {entities_throughput}")
 
         # t0 = time.time()
