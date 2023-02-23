75bb9b205dc074b7f6b1dea3905d901527076a18
yanliang567 <yanliang.qiao@zilliz.com>
2023-02-23 10:17:51 +0800

    Update for multi resource groups support
    Signed-off-by: yanliang567 <yanliang.qiao@zilliz.com>

--- tests/python_client/bricks/create_n_load_rg.py
+++ tests/python_client/bricks/create_n_load_rg.py
@@ -6,6 +6,7 @@ import logging
 from pymilvus import connections, DataType, \
     Collection, FieldSchema, CollectionSchema, utility
 import create_n_insert
+import string
 
 
 LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
@@ -13,55 +14,19 @@ DATE_FORMAT = "%m/%d/%Y %H:%M:%S %p"
 
 default_rg = "__default_resource_group"
 
-#
-# def create_n_insert(collection_name, index_type, metric_type="L2"):
-#     id_field = FieldSchema(name="id", dtype=DataType.INT64, description="auto primary id")
-#     age_field = FieldSchema(name="age", dtype=DataType.INT64, description="age")
-#     embedding_field = FieldSchema(name="embedding", dtype=DataType.FLOAT_VECTOR, dim=dim)
-#     schema = CollectionSchema(fields=[id_field, age_field, embedding_field],
-#                               auto_id=auto_id, primary_field=id_field.name,
-#                               description=f"{collection_name}")
-#     collection = Collection(name=collection_name, schema=schema, shards_num=shards)
-#     logging.info(f"create {collection_name} successfully")
-#
-#     for i in range(insert_times):
-#         # prepare data
-#         ages = [random.randint(1, 100) for _ in range(nb)]
-#         embeddings = [[random.random() for _ in range(dim)] for _ in range(nb)]
-#         data = [ages, embeddings]
-#         t0 = time.time()
-#         collection.insert(data)
-#         tt = round(time.time() - t0, 3)
-#         logging.info(f"insert {i} costs {tt}")
-#
-#     collection.flush()
-#     logging.info(f"collection entities: {collection.num_entities}")
-#
-#     index_params_dict = {
-#         "HNSW": {"index_type": "HNSW", "metric_type": metric_type, "params": {"M": 8, "efConstruction": 96}},
-#         "DISKANN": {"index_type": "DISKANN", "metric_type": metric_type, "params": {}}
-#     }
-#     index_params = index_params_dict.get(index_type.upper(), None)
-#     if index_params is None:
-#         logging.error(f"index type {index_type} no supported")
-#         exit(1)
-#
-#     if not collection.has_index():
-#         t0 = time.time()
-#         collection.create_index(field_name=embedding_field.name, index_params=index_params)
-#         tt = round(time.time() - t0, 3)
-#         logging.info(f"build index {index_params} costs {tt}")
-#     else:
-#         idx = collection.index()
-#         logging.info(f"index {idx.params} already exists")
-#
+
+def gen_unique_str(str_value=None):
+    prefix = "".join(random.choice(string.ascii_letters + string.digits) for _ in range(8))
+    return prefix if str_value is None else str_value + "_" + prefix
+
 
 if __name__ == '__main__':
     host = sys.argv[1]  # host address
     name = str(sys.argv[2])  # collection name
     index = str(sys.argv[3])  # index type
     metric = str(sys.argv[4])  # metric type
-    rg_name = str(sys.argv[5])  # resource group name
+    rg_num = int(sys.argv[5])  # resource group num == replica num
+    node_num_each_rg = int(sys.argv[6])  # query node num for each rg
     port = 19530
     log_name = f"prepare_{name}"
 
@@ -76,25 +41,24 @@ if __name__ == '__main__':
     connections.connect('default')
 
     default_rg_info = utility.describe_resource_group(name=default_rg)
-    rgs = utility.list_resource_groups()
-    if rg_name in rgs:
-        logging.info(f"rg {rg_name} already exists")
-    else:
+    if default_rg_info.num_available_node < rg_num * node_num_each_rg:
+        logging.error(f"there is no available nodes in default rg, expected: {rg_num} x {node_num_each_rg}")
+        exit(0)
+
+    prefix = gen_unique_str()
+    rgs = [f"rg_{i}_{prefix}" for i in range(rg_num)]
+    for rg_name in rgs:
         utility.create_resource_group(name=rg_name)
-    rg_info = utility.describe_resource_group(name=rg_name)
-    if rg_info.num_available_node < 1:
-        if default_rg_info.num_available_node < 1:
-            logging.error(f"there is no available nodes in default rg")
-            exit(0)
-        utility.transfer_node(source=default_rg, target=rg_name, num_node=1)
+        utility.transfer_node(source=default_rg, target=rg_name, num_node=node_num_each_rg)
+        logging.info(f"create and transfer node to resource group {rg_name}")
 
     # create an insert
     create_n_insert.create_n_insert(collection_name=name, index_type=index, metric_type=metric)
 
     # load collection
     c = Collection(name=name)
-    c.load(_resource_groups=[rg_name])
-    logging.info(f"collection loaded to resource groups: {[rg_name]}")
+    c.load(replica_number=rg_num, _resource_groups=rgs)
+    logging.info(f"collection loaded to resource groups: {rgs}")
 
     logging.info("collection prepared completed")
 
